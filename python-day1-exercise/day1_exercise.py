7efd700ab80766866d85b998a6d7dc6d58da2812
ethanl267 <71071509+ethanl267@users.noreply.github.com>
2020-09-21 12:14:54 +0200

    Add files via upload

---
+++ python-day1-exercise/day1_exercise.py
@@ -0,0 +1,20 @@
+#TASK 1: accept an input and display
+# hello (name): how old are you?
+# where (name) is the input
+
+# TASK 2:accept the input of the radius of a circle and the circumference
+#example:
+# Radius:5 // input
+# Circumference: 31.415 //output
+# Formula 2*radius*3.1415
+
+radius =int(input("what is the radius: "))
+pi =3.1415
+
+circumference = 2*radius*pi
+print(circumference)
+
+
+
+
+
